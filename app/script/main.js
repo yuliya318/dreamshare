eb9da1302bfa52518c035ee5e79b2df07b80879a
Yuliya318 <yulya31087@gmail.com>
2020-06-04 21:28:53 +0300

    adding project compiled with gulp

---
+++ app/script/main.js
@@ -0,0 +1,12 @@
+console.log('Gulp works')
+
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.showData = function(){
+        console.log(`Hello ${this.firstName} ${this.lastName}`);
+    }
+}
+
+let newP = new Person('Ivan', 'Ivanov');
+newP.showData();
